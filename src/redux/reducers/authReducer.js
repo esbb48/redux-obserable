27a24cb118d18884e6bae498ec07a88321ac4e28
esbb48 <esbb48@gmail.com>
2020-12-09 14:31:15 +0800

    Add immer

--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -1,13 +1,14 @@
-import { authState } from './initialState';
+import produce from 'immer';
 import types from '../actionTypes';
+import { authState } from './initialState';
 
-const authReducer = (state = authState, action) => {
+const authReducer = produce((state, action) => {
   switch (action.type) {
     case types.GET_LIST_SUCCESS:
       return { ...authState, users: [{ id: 1, name: 'Alice' }] };
     default:
       return state;
   }
-};
+}, authState);
 
 export default authReducer;
